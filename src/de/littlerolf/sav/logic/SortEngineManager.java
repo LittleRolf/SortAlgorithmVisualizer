a9c24c5f2961df4893073ae361d4cee9fe685712
Ole Wehrmeyer <ole.wehrmeyer@ewetel.net>
2014-02-27 22:32:44 +0100

    first logic commit

---
+++ src/de/littlerolf/sav/logic/SortEngineManager.java
@@ -0,0 +1,8 @@
+package de.littlerolf.sav.logic;
+
+import de.littlerolf.sav.loader.SorterLoader;
+
+public class SortEngineManager {
+	private SorterLoader sl;
+	//TODO: heavy important stuff for tomorrow
+}
