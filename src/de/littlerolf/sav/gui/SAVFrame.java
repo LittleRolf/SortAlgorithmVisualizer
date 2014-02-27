581176e8afeb5c02803f5ca8555f05b9738acfde
Jan-Henrik <jh.bruhn@me.com>
2014-02-27 18:05:02 +0100

    removed some random shit

--- src/de/littlerolf/sav/gui/SAVFrame.java
+++ src/de/littlerolf/sav/gui/SAVFrame.java
@@ -83,16 +83,11 @@ public class SAVFrame extends JFrame {
 
 	public static void main(String[] args) {
 		try {
-			// Set System L&F
 			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
 		} catch (UnsupportedLookAndFeelException e) {
-			// handle exception
 		} catch (ClassNotFoundException e) {
-			// handle exception
 		} catch (InstantiationException e) {
-			// handle exception
 		} catch (IllegalAccessException e) {
-			// handle exception
 		}
 		new SAVFrame().setVisible(true);
 	}
