2772f2535181f3461541e8f98f065e2eadb1f587
Ole Wehrmeyer <ole.wehrmeyer@ewetel.net>
2014-02-27 19:58:04 +0100

    Initial commit of SorterLoader
    Started with basic functions, still untested!

---
+++ src/de/littlerolf/sav/loader/SorterLoader.java
@@ -0,0 +1,77 @@
+package de.littlerolf.sav.loader;
+
+import java.io.File;
+import java.io.FilenameFilter;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.util.ArrayList;
+import java.util.List;
+
+
+/**
+ * The Main Class of the Loader module, used for loading and accessing the specific Sorters
+ * @author ole
+ *
+ */
+public class SorterLoader {
+
+	private final String classpath;
+	
+	private List<Class> classes = new ArrayList<Class>();
+	
+	public SorterLoader(String path) {
+		classpath = path;
+		
+	}
+	public void loadAllClasses() {
+		// Create a File object on the root of the directory containing the class file
+		File file = new File(classpath);
+
+		try {
+		    // Convert File to a URL
+		    URL url = file.toURI().toURL();          // file:/c:/myclasses/
+		    URL[] urls = new URL[]{url};
+
+		    // Create a new class loader with the directory
+		    ClassLoader cl = new URLClassLoader(urls);
+
+		    String[] directories = getSubdirectories();
+		    
+		    for(String folder : directories) {
+		    	classes.add(cl.loadClass(folder + ".Sorter"));
+		    }
+
+		    
+		} catch (MalformedURLException e) {
+		} catch (ClassNotFoundException e) {
+		}
+	}
+	
+	public Class getClassByPackageName(String packageName) {
+		for(Class c : classes) {
+			if(c.getPackage().getName().equals(packageName)) {
+				return c;
+			}
+		}
+		return null;
+	}
+	
+	public String[] getAvailablePackagesList() {
+		String[] packages = new String[classes.size()];
+		for(int i = 0; i < classes.size()-1;i++) {
+			packages[i] = classes.get(i).getPackage().getName();
+		}
+		return packages;
+	}
+	
+	private String[] getSubdirectories() {
+	    File file = new File(classpath);
+	    String[] directories = file.list(new FilenameFilter() {
+	      public boolean accept(File current, String name) {
+	        return new File(current, name).isDirectory();
+	      }
+	    });
+	    return directories;
+	}
+}
