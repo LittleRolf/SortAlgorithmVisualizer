12ef4c04f43666e1ff64bb6914468cf533594122
Ole Wehrmeyer <ole.wehrmeyer@ewetel.net>
2014-02-27 21:40:14 +0100

    Tested class loading functionality

--- src/de/littlerolf/sav/loader/SorterLoader.java
+++ src/de/littlerolf/sav/loader/SorterLoader.java
@@ -8,79 +8,106 @@ import java.net.URLClassLoader;
 import java.util.ArrayList;
 import java.util.List;
 
-
 /**
- * The Main Class of the Loader module, used for loading and accessing the specific Sorters
+ * The Main Class of the Loader module, used for loading and accessing the
+ * specific Sorters
+ * 
  * @author ole
- *
+ * 
  */
 public class SorterLoader {
 
 	private final String classpath;
-	
+	private boolean debug = false;
+
 	private List<Class> classes = new ArrayList<Class>();
-	
+
 	/**
 	 * Creates a new SorterLoader looking for classes in the given path
-	 * @param path The path to look for classes
+	 * 
+	 * @param path
+	 *            The path to look for classes
 	 */
 	public SorterLoader(String path) {
 		classpath = path;
-		
+
 	}
-	
+
 	/**
-	 * Loads all classes named 'Sorter' from the set folder from all the subdirectories
+	 * Creates a new SorterLoader looking for classes in the given path and
+	 * offers ability to enable debug output
+	 * 
+	 * @param path
+	 *            The path to look for classes
+	 * @param debug
+	 *            Debug Flag
+	 */
+	public SorterLoader(String path, boolean debug) {
+		classpath = path;
+		this.debug = debug;
+	}
+
+	/**
+	 * Loads all classes named 'Sorter' from the set folder from all the
+	 * subdirectories
 	 */
 	public void loadAllClasses() {
-		// Create a File object on the root of the directory containing the class file
+		// Create a File object on the root of the directory containing the
+		// class file
 		File file = new File(classpath);
 
 		try {
-		    // Convert File to a URL
-		    URL url = file.toURI().toURL();          // file:/c:/myclasses/
-		    URL[] urls = new URL[]{url};
+			// Convert File to a URL
+			URL url = file.toURI().toURL(); // file:/c:/myclasses/
+			URL[] urls = new URL[] { url };
 
-		    // Create a new class loader with the directory
-		    ClassLoader cl = new URLClassLoader(urls);
+			// Create a new class loader with the directory
+			ClassLoader cl = new URLClassLoader(urls);
 
-		    String[] directories = getSubdirectories();
-		    
-		    
-		    for(String folder : directories) {
-		    	classes.add(cl.loadClass(folder + ".Sorter"));
-		    }
+			String[] directories = getSubdirectories();
 
-		    
+			for (String folder : directories) {
+				classes.add(cl.loadClass(folder + ".Sorter"));
+			}
+
 		} catch (MalformedURLException e) {
 		} catch (ClassNotFoundException e) {
 		}
+
+		if (debug) {
+			System.out.println("[Debug] Loaded Classes:");
+			System.out.println("____________________");
+			for (Class c : classes) {
+				System.out.println(c.getPackage().getName() + "    "
+						+ c.getName());
+			}
+		}
 	}
-	
-	public Class getClassByPackageName(String packageName) {
-		for(Class c : classes) {
-			if(c.getPackage().getName().equals(packageName)) {
+
+	public Class getSorterByPackageName(String packageName) {
+		for (Class c : classes) {
+			if (c.getPackage().getName().equals(packageName)) {
 				return c;
 			}
 		}
 		return null;
 	}
-	
+
 	public String[] getAvailablePackagesList() {
 		String[] packages = new String[classes.size()];
-		for(int i = 0; i < classes.size()-1;i++) {
+		for (int i = 0; i < classes.size() - 1; i++) {
 			packages[i] = classes.get(i).getPackage().getName();
 		}
 		return packages;
 	}
-	
+
 	private String[] getSubdirectories() {
-	    File file = new File(classpath);
-	    String[] directories = file.list(new FilenameFilter() {
-	      public boolean accept(File current, String name) {
-	        return new File(current, name).isDirectory();
-	      }
-	    });
-	    return directories;
+		File file = new File(classpath);
+		String[] directories = file.list(new FilenameFilter() {
+			public boolean accept(File current, String name) {
+				return new File(current, name).isDirectory();
+			}
+		});
+		return directories;
 	}
 }
