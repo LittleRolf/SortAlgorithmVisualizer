be2381db47f07b7ae5f42d31b07378a3ca3c4a6a
Belogron <olewehrmeyer@web.de>
2014-03-03 12:12:14 +0100

    Finalised SorterLoader, deleted obsolete Loader
    Heavy shit...

--- src/de/littlerolf/sav/logic/SortEngineManager.java
+++
@@ -1,8 +0,0 @@
-package de.littlerolf.sav.logic;
-
-import de.littlerolf.sav.loader.SorterLoader;
-
-public class SortEngineManager {
-	private SorterLoader sl;
-	//TODO: heavy important stuff for tomorrow
-}
