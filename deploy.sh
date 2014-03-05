c0ccf45a9bb994c295dbfc9b48b06148886604ec
Jan-Henrik Bruhn <jh.bruhn@me.com>
2014-03-05 18:06:14 +0100

    corrected deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 if [ ${TRAVIS_JOB_NUMBER##*.} -eq 1 ]; then
-	pip install ghp-import
+	sudo pip install ghp-import
 	ghp-import -n dist/ -m "Deploy ${TRAVIS_BUILD_NUMBER}."
-  git push -fq https://${GIT_TOKEN}@github.com/$(TRAVIS_REPO_SLUG).git gh-pages > /dev/null
+  git push -fq https://$GIT_TOKEN@github.com/$TRAVIS_REPO_SLUG.git gh-pages > /dev/null
 fi
