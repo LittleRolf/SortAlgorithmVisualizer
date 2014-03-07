3b72f256ff22aaa8ca128a91d6ac500cbf96fb0f
Jan-Henrik <jh.bruhn@me.com>
2014-03-07 20:33:11 +0100

    Added ProgressFrame

---
+++ src/de/littlerolf/sav/gui/ProgressFrame.java
@@ -0,0 +1,39 @@
+package de.littlerolf.sav.gui;
+
+import javax.swing.JFrame;
+import javax.swing.JProgressBar;
+import java.awt.BorderLayout;
+
+public class ProgressFrame extends JFrame {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	JProgressBar progressBar;
+
+	public ProgressFrame() {
+		setResizable(false);
+		setTitle("Doing awesome shit.");
+
+		progressBar = new JProgressBar();
+		progressBar.setStringPainted(true);
+		getContentPane().add(progressBar, BorderLayout.CENTER);
+		this.setSize(400, 100);
+	}
+
+	public void setString(String s) {
+		this.progressBar.setString(s);
+	}
+
+	public void setMaximum(int max) {
+		this.progressBar.setMaximum(max);
+	}
+
+	public void setMinimum(int min) {
+		this.progressBar.setMinimum(min);
+	}
+
+	public void setValue(int v) {
+		this.progressBar.setValue(v);
+	}
+}
